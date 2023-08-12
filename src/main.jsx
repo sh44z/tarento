421d589f4075f4ce02cd485601f064b1f65cfa5c
Shehzad Khan <sh44z@live.co.uk>
2023-08-12 12:43:40 +0100

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,10 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
+const githubPagesUrl = 'https://sh44z.github.io/tarento/';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename={githubPagesUrl}>
   <React.StrictMode>
     <App />
   </React.StrictMode>
